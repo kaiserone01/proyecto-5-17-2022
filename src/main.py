4f3c9dff3faa3f6cf72e18a9a9ed0242bb7e8e46
kaiserone01 <mrporteles@gmail.com>
2022-05-17 18:20:15 +0000

    cambios

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Personajes, Planeta
 #from models import Person
 
 app = Flask(__name__)
@@ -26,18 +26,99 @@ def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    # POST request
+        body = request.get_json() # get the request body content
+        if body is None:
+            return "The request body is null", 400
+        if 'nombre' not in body:
+            return 'You need to specify the nombre',400
+        if 'email' not in body:
+            return 'You need to specify the email', 400
+        if 'password' not in body:
+            return 'You need to specify the password', 400   
+       
+
+        newUser=User(nombre=body["nombre"],email=body["email"],password=body["password"],is_active=True)
+        db.session.add(newUser)
+        db.session.commit()
+        
+    
+
+        return jsonify(newUser.serialize()), 200
+
+@app.route('/user', methods=['GET'])
+def listausuarios():
+    user = User.query.all()
+    user_serializado = list(map(lambda user: user.serialize(),user))
+    return jsonify(user_serializado), 200
+
+
+@app.route('/personajes', methods=['POST'])
+def create_person():
+    # POST request
+        body = request.get_json() # get the request body content
+        if body is None:
+            return "The request body is null", 400
+        if 'nombre' not in body:
+            return 'You need to specify the first_name',400
+        if 'tipo' not in body:
+            return 'You need to specify the last_name', 400
+        if 'poder' not in body:
+            return 'You need to specify the last_name', 400
+
+        newPersonaje=Personajes(nombre=body["nombre"], tipo=body["tipo"], poder=body["poder"])
+        db.session.add(newPersonaje)
+        db.session.commit()
+        
+    
+
+        return jsonify(newPersonaje.serialize()), 200
+
+@app.route('/personajes', methods=['GET'])
+def listapersonajes():
+    personajes = Personajes.query.all()
+    personajes_serializado = list(map(lambda personaje: personaje.serialize(),personajes))
+    return jsonify(personajes_serializado), 200
+
+@app.route('/planetas', methods=['POST'])
+def create_planeta():
+    # POST request
+        body = request.get_json() # get the request body content
+        if body is None:
+            return "The request body is null", 400
+        if 'nombre' not in body:
+            return 'You need to specify the nombre',400
+        if 'habitable' not in body:
+            return 'You need to specify the habitable', 400
+        if 'clima' not in body:
+            return 'You need to specify the clima', 400
+
+        newPlaneta=Planeta(nombre=body["nombre"], habitable=body["habitable"], clima=body["clima"])
+        db.session.add(newPlaneta)
+        db.session.commit()
+        
+    
+
+        return jsonify(newPlaneta.serialize()), 200
+
+@app.route('/planetas', methods=['GET'])
+def listadeplanetas():
+    planeta = Planeta.query.all()
+    planeta_serializado = list(map(lambda planeta: planeta.serialize(),planeta))
+    return jsonify(planeta_serializado), 200
+    
+
+
+
+
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
-
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,8 +4,9 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    nombre = db.Column(db.String(120), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=False)
+    password = db.Column(db.String(200), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -14,6 +15,44 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "nombre": self.nombre,
             "email": self.email,
+            "password": self.password,
             # do not serialize the password, its a security breach
+        }
+
+class Personajes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), unique=True, nullable=False)
+    poder = db.Column(db.String(120), unique=False, nullable=False)
+    tipo = db.Column(db.String(120), unique=False, nullable=False)
+   
+
+    def __repr__(self):
+        return '<Personajes %r>' % self.username
+
+    def serialize(self):
+        return {
+            "nombre": self.nombre,
+            "poder": self.poder,
+            "tipo": self.tipo,
+
+        }
+        
+class Planeta(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), unique=True, nullable=False)
+    habitable = db.Column(db.String(120), unique=False, nullable=False)
+    clima = db.Column(db.String(120), unique=False, nullable=False)
+
+
+    def __repr__(self):
+        return '<Planeta %r>' % self.username
+
+    def serialize(self):
+        return {
+            "nombre": self.nombre,
+            "habitale": self.habitable,
+            "clima": self.clima,
+
         }
